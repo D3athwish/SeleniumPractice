17b21e5189b48875fc87fe367c9910d7ff8d668c
D3athwish <61936623+D3athwish@users.noreply.github.com>
2021-06-07 02:46:15 +0200

    Bug Present: ESC at match case window

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,10 +2,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,38 +13,21 @@ import java.util.List;
 
 
 class MainTest extends RestAssured{
-
-    public final int TIME_SLEEP = 5000;
-    final String getURL = "https://reqres.in/api/users";
+    // Sleep has to be extended to 5 seconds - Employee forms are ignoring await...
+    public final int TIME_SLEEP = 10000;
     List<String> employeeNameList = new ArrayList<>();
 
-    // TODO: Ok so the below code now works great, but I have to structure this better...
-    //  1. Move the REST API to a Service Method and call it, the method should return the arrayList of names
-    //  2. Next, fully implement Selenium -> shouldn't be that hard to do, again follow structuring
-
-
-    public int getEmployeeCount() throws Exception{
-        final var response = given()
-                .get(getURL)
-                .then()
-                .contentType(ContentType.JSON)
-                .extract().response();
-
-        // Get the number of employees, and then append these employees to an arraylist
-        return response.jsonPath().getInt("data.size()");
-    }
-
-
     @Test
-    public void getEmployeesNames() throws Exception{
-        // OK! Finally something works! :)
+    public void testing_employee_insertion() throws Exception{
+        // Extract JSON to response variable
         final var response = given()
-                .get(getURL)
+                .get("https://reqres.in/api/users")
                 .then()
                 .contentType(ContentType.JSON)
-                .extract().response();
+                .extract()
+                .response();
 
-        // Get the number of employees, and then append these employees to an arraylist
+        // Loop over all employees and add all first names to employeeNameList
         int employeeCount = response.jsonPath().getInt("data.size()");
         for(int i = 0; i < employeeCount; i++){
             employeeNameList.add(response.jsonPath().getString(String.format("data[%s].first_name", i)));
@@ -56,92 +36,74 @@ class MainTest extends RestAssured{
         if(employeeCount <= 0){
             Assertions.fail("No employees!");
         }else{
-            login();
+            chromeDriverStart();
         }
     }
 
-    // Starts WebDriver, launches URL -> Enters login information to access website
-    public void login() throws Exception{
+    // Starts Chrome WebDriver, and begins in this order: login -> navigate to employees -> inserts new employees
+    public void chromeDriverStart() throws Exception{
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        String emailLoginBox = "#i0116";
-        String nextLoginButton = "#idSIButton9";
-        String passwordSelector = "#i0118";
-        String signInLoginSelector = "#idSIButton9";
-        String skipXPATH = "/html/body/div/form[1]/div/div/div[1]/div/div[3]/div/div[2]/div/div[3]/a";
-        String staySignedInSelector = "#idSIButton9";
-        String websiteURL = "https://trial-7v8nl0.trial.operations.dynamics.com/";
-
         WebDriver driver = new ChromeDriver();
-        // Hmm... couldn't find a way to set this globally... Looks like it has to be defined in each individual function
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         // Open Microsoft Login page, and wait for login element to be present
-        driver.get(websiteURL);
-        waitAndClick(driver, emailLoginBox);
+        driver.get("https://trial-7v8nl0.trial.operations.dynamics.com/");
+        waitAndFindCSS(driver, "#i0116")
+                .click();
 
         // Login: Insert Email
-        driver.findElement(By.cssSelector(emailLoginBox)).sendKeys("ogris@paurus.si");
+        waitAndFindCSS(driver, "#i0116")
+                .sendKeys("ogris@paurus.si");
 
         // Login: Click next
-        driver.findElement(By.cssSelector(nextLoginButton)).click();
+        waitAndFindCSS(driver, "#idSIButton9")
+                .click();
 
         // Login: Insert Password
-        waitAndClick(driver, passwordSelector);
-        driver.findElement(By.cssSelector(passwordSelector)).sendKeys("8bvFcKRkMgP7Y7W");
+        waitAndFindCSS(driver, "#i0118")
+                .click();
+        waitAndFindCSS(driver, "#i0118")
+                .sendKeys("8bvFcKRkMgP7Y7W");
 
         // Login: Clicking Sign In
-        driver.findElement(By.cssSelector(signInLoginSelector)).click();
+        waitAndFindCSS(driver,"#idSIButton9")
+                .click();
 
-        // Login: Clicking skip for now
-        driver.findElement(By.xpath(skipXPATH)).click();
+        // Login: Clicking skip for now - having issues with using selector method so using xpath
+        waitAndFindXPath(driver, "/html/body/div/form[1]/div/div/div[1]/div/div[3]/div/div[2]/div/div[3]/a")
+                .click();
 
-        // Login: Click Yes - Stay signed in?
-        // Not 100% sure if we really need a try/finally here, sometimes it asks sometimes it doesn't
-        try {
-            waitAndClick(driver, staySignedInSelector);
-        } finally {
-            navigateToEmployees(driver);
-        }
+        // Login: Click Yes - Stay signed in
+        waitAndFindCSS(driver, "#idSIButton9")
+                .click();
+
+        navigateToEmployees(driver);
     }
 
-    // This entire expression to wait for a certain element is present is a little long, shortening it with a function...
-    public void waitAndClick(WebDriver driver, String cssSelector) throws Exception{
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
-        driver.findElement(By.cssSelector(cssSelector)).click();
-    }
-
-    public void waitAndSendKeys(WebDriver driver, String cssSelector, String keys) throws Exception{
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
-        driver.findElement(By.cssSelector(cssSelector)).sendKeys(keys);
-    }
-
-    private void navigateToEmployees(WebDriver driver) throws Exception {
-        waitAndClick(driver, "#DefaultDashboard_2_CompanyLogoTop > span > img");
+    // After login is complete navigate to employees screen
+    private void navigateToEmployees(WebDriver driver) {
+        waitAndFindCSS(driver, "#DefaultDashboard_2_CompanyLogoTop > span > img");
 
         // navigateToEmployees: Open siderbar with shortcut combo: ALT + F1
         waitAndSendKeys(driver, "body", Keys.chord(Keys.ALT, Keys.F1));
 
-        // navigateToEmployees: Click modules
-        waitAndClick(driver, "#navPaneModuleID");
+        // navigateToEmployees: Click modules - Weird interaction on this element, do not resize screen
+        waitAndFindCSS(driver, "#navPaneModuleID")
+                .click();
 
         // navgiateToEmployees: Clicking Human Resources
-        waitAndClick(driver, "#mainPane > div.modulesPane > div > div.gutterList.showFullGutter > div.modulesList.modulesExpanded > a:nth-child(20)");
+        waitAndFindCSS(driver, "#mainPane > div.modulesPane > div > div.gutterList.showFullGutter" +
+                " > div.modulesList.modulesExpanded > a:nth-child(20)")
+                .click();
 
-        // navigateToEmployees: Clickinng Human Resources -> Employees
-        waitAndClick(driver, "#mainPane > div.modulesPane > div > div.modulesPane-flyout.slideInL > div > div.modulesFlyout-container > div:nth-child(9)");
+        // navigateToEmployees: Clicking Human Resources -> Employees
+        waitAndFindCSS(driver, "#mainPane > div.modulesPane > div > div.modulesPane-flyout.slideInL > div" +
+                " > div.modulesFlyout-container > div:nth-child(9)")
+                .click();
 
         insertEmployees(driver);
     }
 
-    private void insertEmployees(WebDriver driver) throws Exception {
-        // TODO:
-        //  1. Click New
-        //  2. Input First name
-        //  3. Input first_name
-        //  4. Pick todays employement start date
-
+    private void insertEmployees(WebDriver driver) {
         employeeNameList.forEach(name -> {
             try {
                 addEmployee(driver, name);
@@ -156,49 +118,67 @@ class MainTest extends RestAssured{
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
         // addEmployee: Click New
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[6]/div/form[2]/div[2]/div[1]/div[3]/button[1]"))).click();
-
-        Thread.sleep(TIME_SLEEP);
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[6]/div/form[2]/div[2]/div[1]/div[3]/button[1]")
+                .click();
 
         // Click calendar icon to input date
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div"))).click();
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[2]/div[2]" +
+                "/div[2]/div[1]/div/div[2]/div")
+                .click();
 
-        Thread.sleep(TIME_SLEEP);
         // Click today
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[9]/div[3]/button[2]"))).click();
+        waitAndFindXPath(driver, "/html/body/div[9]/div[3]/button[2]")
+                .click();
 
-        Thread.sleep(TIME_SLEEP);
         // addEmployee: Click first_name and input the name
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/input"))).click();
-        Thread.sleep(TIME_SLEEP);
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/input"))
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]" +
+                "/div[1]/div[1]/div[2]/div/input")
+                .click();
+
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]" +
+                "/div[1]/div[1]/div[2]/div/input")
                 .sendKeys(first_name);
 
-        Thread.sleep(TIME_SLEEP);
-
         // Click middle name so Hire button becomes active
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div/input"))).click();
-
-        Thread.sleep(TIME_SLEEP);
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[1]/div[1]/div[2]" +
+                "/div[1]/div[2]/div/input")
+                .click();
 
         // Finally click Hire
-        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[2]/div[2]/button[1]"))).click();
+        waitAndFindXPath(driver, "/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div[2]/div[2]/div[2]/button[1]")
+                .click();
 
-        Thread.sleep(TIME_SLEEP);
+        Thread.sleep(10000);
 
-        // Click cancel after hire is clicked
-
-        List<WebElement> cancelElement = driver.findElements(By.xpath("/html/body/div[2]/div/div[8]/div[2]/div/div[4]/div[2]/div[2]/div[2]/button[2]"));
+        // The idea here is to check if a cancel button is present, if it is we use the ESC key to close the menu
+        // Having issues with detecting this...
+        List<WebElement> cancelElement = driver
+                .findElements(By.xpath("/html/body/div[2]/div/div[7]/div[2]/div/div[4]/div/div[2]/div[2]/button[3]"));
         if(cancelElement.size() > 0){
-            new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[8]/div[2]/div/div[4]/div[2]/div[2]/div[2]/button[2]")))
-                    .click();
+            waitAndFindCSS(driver, "#DirPartyVerification_10_DirPartyTable_Name_input").click();
+            waitAndSendKeys(driver, "#DirPartyVerification_10_DirPartyTable_Name_input", Keys.chord(Keys.ESCAPE));
         }
+    }
+
+    // Wait till (CSS) element is present then continue...
+    public WebElement waitAndFindCSS(WebDriver driver, String cssSelector) {
+        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
+        return driver.findElement(By.cssSelector(cssSelector));
+    }
+
+    // Adding Thread.sleep here, WebDriverWait doesn't seem to work consistently on Employee Forms :/
+    public WebElement waitAndFindXPath(WebDriver driver, String XPath) throws InterruptedException {
+        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPath)));
+        Thread.sleep(TIME_SLEEP);
+        return driver.findElement(By.xpath(XPath));
+    }
+
+    // Wait till element is present then continue adding keys...
+    public void waitAndSendKeys(WebDriver driver, String cssSelector, String keys) {
+        new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds())
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
+        driver.findElement(By.cssSelector(cssSelector)).sendKeys(keys);
     }
 }
